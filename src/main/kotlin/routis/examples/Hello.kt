e11a70e192e6e083741b513cb7b394ec89f269bd
babis <haralampos.routis@intrasoft-intl.com>
2018-05-08 13:16:25 +0300

    Code commited

---
+++ src/main/kotlin/routis/examples/Hello.kt
@@ -0,0 +1,15 @@
+package routis.examples
+
+import arrow.instance
+import arrow.typeclasses.Monoid
+import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
+
+data class BigDecimalWrapper(val value: BigDecimal)
+
+@instance(BigDecimalWrapper::class)
+interface BigDecimalAddition : Monoid<BigDecimalWrapper>{
+    override fun empty(): BigDecimalWrapper  = BigDecimalWrapper(ZERO)
+    override fun BigDecimalWrapper.combine(b: BigDecimalWrapper): BigDecimalWrapper =
+        BigDecimalWrapper(this.value.plus(b.value))
+}
